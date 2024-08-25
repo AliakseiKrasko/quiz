3451160f119c032a240d3f972ff4e6d5b36edb2f
krasko <alekskrasko1985@gmail.com>
2024-08-25 20:40:47 +0300

    add card stock transfer

--- Desktop/startovyj-kod-proekta (1)/quiz/js/quiz.js
+++ Desktop/startovyj-kod-proekta (1)/quiz/js/quiz.js
@@ -1,1 +1,34 @@
-console.log('Hello, world!)');
+const cards = document.querySelectorAll('.plate');
+
+let currentCard = 0;
+
+cards[currentCard].classList.add('visible')
+
+
+window.addEventListener('click', function(event) {
+
+    if (event.target.closest('[data-nav="next"]')) {
+       
+        //скрываем текущую карточку
+        cards[currentCard].classList.remove('visible');
+
+        // меняем индекс текущей карточки
+        currentCard = currentCard + 1;
+
+        // показываем новую карточку
+        cards[currentCard].classList.add('visible');
+    }
+
+    if (event.target.closest('[data-nav="prev"]')) {
+        
+        // скрываем текущую карточку
+        cards[currentCard].classList.remove('visible');
+
+        // меняем индекс текущей карточки
+        currentCard = currentCard - 1;
+
+        // показываем новую карточку
+        cards[currentCard].classList.add('visible');
+    }
+    
+});
