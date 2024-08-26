16da98ac1a87a2874a7bc70130e3750929a653c4
krasko <alekskrasko1985@gmail.com>
2024-08-26 23:15:46 +0300

    added modifications

--- Desktop/startovyj-kod-proekta (1)/quiz/js/quiz.js
+++ Desktop/startovyj-kod-proekta (1)/quiz/js/quiz.js
@@ -1,11 +1,17 @@
 const cards = document.querySelectorAll('.plate');
 
-let currentCard = 0;
+cards.forEach(function(card) {card.classList.add('none')}); 
+
+let currentCard = 0; // для перемещений по карточкам
+let currentCardIndex = 0; // для прогресс бара
 
 // скрываем кнопку назад на первой карточке
 cards[0].querySelector('[data-nav="prev"]').remove();
 
+cards[currentCard].classList.remove('none');
 cards[currentCard].classList.add('visible');
+
+updateProgressBar();
 
 window.addEventListener('click', function(event) {
 
@@ -15,41 +21,57 @@ window.addEventListener('click', function(event) {
         const answersWrapper = cards[currentCard].querySelector('[data-answers]');
 
         if (result) {
+            // увеличиваем индекс текущего прогресс бара
+            updateProgressBar('next');
+            
+            setTimeout(function () {
+                // скрываем текущую карточку
+                cards[currentCard].classList.remove('visible');
         
-            // скрываем текущую карточку
-            cards[currentCard].classList.remove('visible');
-
-            // меняем индекс текущей карточки
-            currentCard = currentCard + 1;
-
-            // удаляем класс 'required' и форму об ошибке
-            answersWrapper.classList.remove('required');
-
-            // показываем новую карточку
-            if (currentCard < cards.length) {
-                cards[currentCard].classList.add('visible');
-                
-            }
+                setTimeout(function () {
+                    // добавляем класс 'none' для скрытия текущей карточки
+                    cards[currentCard].classList.add('none');
+        
+                    // меняем индекс текущей карточки
+                    currentCard = currentCard + 1;
+        
+                    // удаляем класс 'required' и форму об ошибке
+                    answersWrapper.classList.remove('required');
+        
+                    // показываем новую карточку
+                    if (currentCard < cards.length) {
+                        cards[currentCard].classList.remove('none'); // удаляем класс 'none' с новой карточки
+                        cards[currentCard].classList.add('visible'); // добавляем класс 'visible' к новой карточке
+                    }
+                }, 100); // задержка перед переключением карточек
+            }, 500); // задержка перед скрытием текущей карточки
+        
         } else {
             console.log('error');
             // добавляем класс 'required' иформация об ошибке
             answersWrapper.classList.add('required');
         }
-    }
+    }        
 
     if (event.target.closest('[data-nav="prev"]')) {
+
+        updateProgressBar('prev');
         
-        if (currentCard == 0) return;
+        setTimeout(function() {
+            if (currentCard == 0) return;
             
-        // скрываем текущую карточку
-        cards[currentCard].classList.remove('visible');
+            // скрываем текущую карточку
+            cards[currentCard].classList.remove('visible');
 
-        // меняем индекс текущей карточки
-        currentCard = currentCard - 1;
+            // меняем индекс текущей карточки
+            currentCard = currentCard - 1;
 
-        // показываем новую карточку
-        cards[currentCard].classList.add('visible');
-    }
+            // показываем новую карточку
+            cards[currentCard].classList.add('visible');
+            
+        }, 500);
+    };
+    
 });
 
 function checkAnswer(currentCardElement) {
@@ -71,14 +93,53 @@ function checkAnswer(currentCardElement) {
     return false;
 }
 
-function updateProgressBar() {
-    const progressValue = document.querySelector('.progress__label strong');
+function updateProgressBar(direction = 'start') {
+
+    
+    if (direction === 'next') {
+        currentCardIndex = currentCardIndex + 1;
+    } else if (direction === 'prev') {
+        currentCardIndex = currentCardIndex - 1;
+    } 
+
+
+
+
+    const progressValue = document.querySelectorAll('.progress__label strong');
     console.log(progressValue);
 
 
-    const progressBar = document.querySelector('.progress__line-bar');
+    const progressBar = document.querySelectorAll('.progress__line-bar');
     console.log(progressBar);
+
+    const countProgCards = document.querySelectorAll('[data-progress]').length;
+    console.log(countProgCards);
+
+    const progress = Math.round((currentCardIndex * 100) / countProgCards);
+    console.log(progress);
+
+
+    progressValue.forEach(function(item) {
+        item.innerText = progress + '%';
+    });
+    
+    progressBar.forEach(function(item) {
+        item.style.width = progress + '%';
+    });
     
 }
 
+
 updateProgressBar();
+
+const checkBoxPolicy = document.querySelector('#policy');
+
+checkBoxPolicy.addEventListener('focus', function() {
+    
+    this.closest('label').classList.add('hovered');
+});
+
+checkBoxPolicy.addEventListener('blur', function() {
+    
+    this.closest('label').classList.remove('hovered');
+});
